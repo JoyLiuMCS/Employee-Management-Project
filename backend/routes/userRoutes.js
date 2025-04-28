a928f1e2274f3c576739fc3b7994d1d144694e40
kiki614zzzz <131100606+kiki614zzzz@users.noreply.github.com>
2025-04-28 14:33:19 -0400

    Update userRoutes.js

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 
-// Route to get all users
-router.get('/users', userController.getAllUsers);
+// GET /api/users   →  getAllUsers
+router.get('/', userController.getAllUsers);
 
-// Route to get a specific user by ID
-router.get('/users/:id', userController.getUserById);
-//router.post('/users', userController.createUser);
+// GET /api/users/:id   →  getUserById
+router.get('/:id', userController.getUserById);
 
 module.exports = router;
